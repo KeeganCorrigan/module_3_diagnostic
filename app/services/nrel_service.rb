a797dd33be0802bff5a86c3beddd6c30930024ef
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-29 10:49:06 -0600

    refactors nrel service model, adds a get url method as well as an instance variable connection

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -2,11 +2,17 @@ class NrelService
   def initialize(zip_code)
     @api_key = "fxoT3RSpBaRGvRIrtQXywpB3eRLaKSKl6asyzNZ8"
     @zip_code = zip_code
+    @conn = Faraday.new(:url => "https://developer.nrel.gov")
   end
 
   def alt_fuel_stations_by_zip_code_location
-    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=#{@zip_code}&radius=6&limit=10&api_key=#{@api_key}"
+    get_url("/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=#{@zip_code}&radius=6&limit=10&api_key=#{@api_key}")
+  end
 
+  private
+
+  def get_url(url)
+    response = @conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
