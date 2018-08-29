79aad3d758d053213c83fd7d0e17903dc5572735
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-29 09:46:07 -0600

    add station model test and add stations to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=80203&limit=10&api_key=fxoT3RSpBaRGvRIrtQXywpB3eRLaKSKl6asyzNZ8"
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=80203&radius=6&limit=10&api_key=fxoT3RSpBaRGvRIrtQXywpB3eRLaKSKl6asyzNZ8"
 
-    
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+
+    binding.pry
+
+    @stations = @stations.map do |station_data|
+      Station.new(station_data)
+    end
   end
 end
